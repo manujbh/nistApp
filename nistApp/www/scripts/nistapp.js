c48efe7e4215ed48d30cff49c798f55e2b46c20d
MB <manujbh@gmail.com>
2018-01-08 21:35:54 -0500

    20180108: basic navigation button with dropdown works.

---
+++ nistApp/www/scripts/nistapp.js
@@ -0,0 +1,31 @@
+﻿window.onload = function () {
+    init();
+}
+
+function init() {
+    var familybtn = document.getElementById("family");
+    familybtn.addEventListener('click', familycontent);
+
+    document.addEventListener('click', hideDropdown);
+
+}
+
+function familycontent(event) {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+function hideDropdown(event) {
+    // Close the dropdown menu if the user clicks outside of it
+    if (!event.target.matches('.dropbtn')) {
+
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
